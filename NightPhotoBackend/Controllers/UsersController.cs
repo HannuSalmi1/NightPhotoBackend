12a35d7dfd9304a5568e617ef172d2f063f3068e
HannuSalmi1 <hannusalmi1@outlook.com>
2023-07-01 02:17:26 +0300

    Add project files.

---
+++ NightPhotoBackend/Controllers/UsersController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NightPhotoBackend.Models;
+
+namespace NightPhotoBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly NightPhotoDbContext _context;
+
+        public UsersController(NightPhotoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsersTable>>> GetUsersTables()
+        {
+          if (_context.UsersTables == null)
+          {
+              return NotFound();
+          }
+            return await _context.UsersTables.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsersTable>> GetUsersTable(int id)
+        {
+          if (_context.UsersTables == null)
+          {
+              return NotFound();
+          }
+            var usersTable = await _context.UsersTables.FindAsync(id);
+
+            if (usersTable == null)
+            {
+                return NotFound();
+            }
+
+            return usersTable;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsersTable(int id, UsersTable usersTable)
+        {
+            if (id != usersTable.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usersTable).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersTableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UsersTable>> PostUsersTable(UsersTable usersTable)
+        {
+          if (_context.UsersTables == null)
+          {
+              return Problem("Entity set 'NightPhotoDbContext.UsersTables'  is null.");
+          }
+            _context.UsersTables.Add(usersTable);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsersTable", new { id = usersTable.Id }, usersTable);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsersTable(int id)
+        {
+            if (_context.UsersTables == null)
+            {
+                return NotFound();
+            }
+            var usersTable = await _context.UsersTables.FindAsync(id);
+            if (usersTable == null)
+            {
+                return NotFound();
+            }
+
+            _context.UsersTables.Remove(usersTable);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsersTableExists(int id)
+        {
+            return (_context.UsersTables?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ NightPhotoBackend/Models/NightPhotoDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NightPhotoBackend.Models;
+
+public partial class NightPhotoDbContext : DbContext
+{
+    public NightPhotoDbContext()
+    {
+    }
+
+    public NightPhotoDbContext(DbContextOptions<NightPhotoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UsersTable> UsersTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-2TA1V08;Initial Catalog=NightPhotoDb;Integrated Security=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UsersTable>(entity =>
+        {
+            entity.ToTable("UsersTable");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(50)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(50)
+                .HasColumnName("lastname");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .HasColumnName("password");
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
